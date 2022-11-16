7e49e86f7c5fbedd4c9f63a4c7dce96fce8898f4
Jeff Kranenburg <super_admin@hamiltonchristian.school.nz>
2022-11-17 07:36:02 +1300

    Updated with prod settings

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,14 +6,22 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-        clientId: "273690688747-j2uaqp8lv6fmivfb91ltrfuopfapu2bi.apps.googleusercontent.com",
-        clientSecret: "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"
+        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
       })
   ],
+  secret: "fddklsajfkjasjfl",
   callbacks: {
     async signIn({ account, profile }) {
       if (account.provider === "google") {
-        return profile.email_verified && (profile.email.endsWith("@hcs.kiwi") || profile.email.endsWith("@hamiltonchristian.school.nz"))
+        return (
+          //Won't check against blanket @hamiltonchristian.school.nz because then students have access to this
+          profile.email_verified && 
+            ( profile.email.endsWith("@hcs.kiwi") || 
+              profile.email("principal@hamiltonchristian.school.nz") ||
+              profile.email("super_admin@hamiltonchristian.school.nz")
+            )
+          )
       }
       return true // Do different verification for other providers that don't have `email_verified`
     },
