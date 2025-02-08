4e6888e50cda94c9ebf83a28105295de2f02674c
Jeff Kranenburg <jwkranenburg@icloud.com>
2025-02-08 17:42:27 +1300

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,7 +9,10 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env': env
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
     server: {
       host: true,
       port: env.VITE_PORT,
